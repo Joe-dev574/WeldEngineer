8742c2df14d85da702e46bc463ca6a9cc45e21e4
JDeWeese <jdeweese574@icloud.com>
2024-08-03 12:09:10 -0500

    Initial Commit

---
+++ WeldEngineer/WeldEngineerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeldEngineerApp.swift
+//  WeldEngineer
+//
+//  Created by Joseph DeWeese on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeldEngineerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
