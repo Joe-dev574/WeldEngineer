aca29b1351abb9cd072d99759c295e260656fa97
JDeWeese <jdeweese574@icloud.com>
2024-08-10 17:05:12 -0500

    investigation

---
+++ WeldEngineer/Models/Investigation.swift
@@ -0,0 +1,8 @@
+//
+//  Investigation.swift
+//  WeldEngineer
+//
+//  Created by Joseph DeWeese on 8/9/24.
+//
+
+import Foundation
---
+++ WeldEngineer/Views/InvestigationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  InvestigationsListView.swift
+//  WeldEngineer
+//
+//  Created by Joseph DeWeese on 8/9/24.
+//
+
+import SwiftUI
+
+struct InvestigationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InvestigationsListView()
+}
